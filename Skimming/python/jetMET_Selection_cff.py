a5642e2917e2c6e7456db78902c552afe62adeb2
edelhoff <>
2009-11-03 21:33:29 +0000

    renamed to fit naming scheme

--- Skimming/python/jetMET_Selection_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-cleanMuons = cms.EDProducer("CandSelector",
-                       src = cms.InputTag("cleanLayer1Muons"),
-                       cut = cms.string("pt > 10 & abs( eta ) < 2.5")
-                       )
-
-seqSelectJetMET = cms.Sequence(cleanMuons)
