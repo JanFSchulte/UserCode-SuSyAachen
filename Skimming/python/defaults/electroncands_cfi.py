f1f46653e446fa27fd2d601f407c2517b77c6c24
nmohr <>
2010-03-02 16:11:42 +0000

    Change default et to 10 GeV

--- Skimming/python/defaults/electroncands_cfi.py
+++ Skimming/python/defaults/electroncands_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 defaultSelector = cms.EDFilter("CandSelector",
         src = cms.InputTag("allSuperClusters"),
-        cut = cms.string('et  > 20.0 & ((abs( eta ) < 1.4442) | (abs( eta ) > 1.560 & abs( eta ) < 2.5) )')
+        cut = cms.string('et  > 10.0 & ((abs( eta ) < 1.4442) | (abs( eta ) > 1.560 & abs( eta ) < 2.5) )')
 )
 
