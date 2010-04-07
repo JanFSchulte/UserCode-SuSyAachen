d179b8ac1a1dfe9553b74c7ee98f05f1899f56a5
sprenger <>
2010-04-07 11:53:22 +0000

    make it compile (need to add ::type)

--- Skimming/plugins/jetSelectors.cc
+++ Skimming/plugins/jetSelectors.cc
@@ -2,23 +2,20 @@
 
 #include "CommonTools/UtilAlgos/interface/ObjectCountFilter.h"
 #include "CommonTools/UtilAlgos/interface/StringCutObjectSelector.h"
-//#include "PhysicsTools/UtilAlgos/interface/MinNumberSelector.h"
-//#include "PhysicsTools/UtilAlgos/interface/MaxNumberSelector.h"
-//#include "PhysicsTools/UtilAlgos/interface/AndSelector.h"
 #include "CommonTools/UtilAlgos/interface/AnySelector.h"
 
 #include "DataFormats/PatCandidates/interface/Jet.h"
 
+
 // define your producer name
 typedef ObjectCountFilter< pat::JetCollection, 
-			   StringCutObjectSelector<pat::Jet> 
-			   > PATJetCountFilter;
+			   StringCutObjectSelector<pat::Jet> >::type PATJetCountFilter;
 
 typedef ObjectCountFilter< edm::View<reco::Candidate>, 
-			   StringCutObjectSelector<reco::Candidate> 
-			   > CandViewJetCountFilter;
+			   StringCutObjectSelector<reco::Candidate> >::type CandViewJetCountFilter;
 
 
 // declare the module as plugin
 DEFINE_FWK_MODULE( PATJetCountFilter );
 DEFINE_FWK_MODULE( CandViewJetCountFilter );
+
