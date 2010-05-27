b73cb1fe1e9d4f44c7d88f9438d1f3695d033510
edelhoff <>
2010-05-27 16:27:38 +0000

    make SUSYPAT additional producers work on 35X data/mc

--- Skimming/python/defaults/SUSYPATdata_cff.py
+++ Skimming/python/defaults/SUSYPATdata_cff.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
-def SUSYPAT(process):  
+def SUSYPATdata(process):  
     from PhysicsTools.Configuration.SUSY_pattuple_cff import addDefaultSUSYPAT, getSUSY_pattuple_outputCommands, removeMCDependence
     process.out = cms.OutputModule("PoolOutputModule",
       fileName = cms.untracked.string('dummy.root'),
       outputCommands = cms.untracked.vstring( 'drop *')
     )
 #    process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
-    addDefaultSUSYPAT(process,True,'HLT','Spring10','35x',['IC5Calo','AK5PF'])
+    addDefaultSUSYPAT(process,False,'HLT','Spring10','35x',['IC5Calo','AK5PF'])
     #addDefaultSUSYPAT(process,True,'HLT','Summer09_7TeV_ReReco332') #no up-to-date JetMET corrections for FastSim these were recomendate somewhere...
     #SUSY_pattuple_outputCommands = getSUSY_pattuple_outputCommands( process ) 
 
     del process.out
-    process.seqSUSYPAT = process.susyPatDefaultSequence
+    process.seqSUSYPATdata = process.susyPatDefaultSequence
                          
