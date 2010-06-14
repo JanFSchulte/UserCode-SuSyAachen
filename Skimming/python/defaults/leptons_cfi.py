d50dab4b794e5c82381d3830c6a4c4cca5d6e783
nmohr <>
2010-06-14 20:27:22 +0000

    Add lepton counter in general form

--- Skimming/python/defaults/leptons_cfi.py
+++ Skimming/python/defaults/leptons_cfi.py
@@ -1,7 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-from SuSyAachen.Skimming.leptonSelectors_cfi import patLeptonCountFilter as patLeptonCountFilterOrig
-patLeptonCountFilter = patLeptonCountFilterOrig.clone(
-           filter = cms.bool(True),
-           minNumber = 1
+from PhysicsTools.PatAlgos.selectionLayer1.leptonCountFilter_cfi import countPatLeptons
+defaultSelector = countPatLeptons.clone(
+    filter = cms.bool(False),
+    electronSource = cms.InputTag("cleanPatElectrons"),
+    muonSource     = cms.InputTag("cleanPatMuons"),
+    tauSource      = cms.InputTag("cleanPatTaus"),
+    countElectrons = cms.bool(True),
+    countMuons     = cms.bool(True),
+    countTaus      = cms.bool(False),
+    minNumber = cms.uint32(0),
+    maxNumber = cms.uint32(999999),
 )
+
