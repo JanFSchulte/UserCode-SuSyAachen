27ecc4c64a8301e27f1d4c3046c152e3ad5b9f8d
nmohr <>
2010-10-14 09:24:50 +0000

    Add new trigger filter

--- Skimming/python/defaults/hlt_cfi.py
+++ Skimming/python/defaults/hlt_cfi.py
@@ -9,3 +9,13 @@ defaultSelector = cms.EDFilter("HLTHighLevel",
     andOr = cms.bool(True),             # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
     throw = cms.bool(True)    # throw exception on unknown path names
 )
+
+triggerSelection = cms.EDFilter( "TriggerResultsFilter",
+    triggerConditions = cms.vstring(),
+    hltResults = cms.InputTag( "TriggerResults" ),
+    l1tResults = cms.InputTag( "" ),
+    l1tIgnoreMask = cms.bool( True ),
+    l1techIgnorePrescales = cms.bool( True ),
+    daqPartitions = cms.uint32( 1 ),
+    throw = cms.bool( True )
+)
