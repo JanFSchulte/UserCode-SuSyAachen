8b0419a82dffac328570d2b492307af86f79ee4b
edelhoff <>
2010-10-24 21:09:07 +0000

    added SignatureFilter - a way to select events with configurable signatures only

--- Skimming/python/defaults/gen_cfi.py
+++ Skimming/python/defaults/gen_cfi.py
@@ -5,3 +5,19 @@ defaultSelector = cms.EDFilter("GenParticleSelector",
         src = cms.InputTag("genParticles"),
         cut = cms.string('abs( eta ) <= 5.0 & pt >= 5')#GeV
 )
+
+signatureFilter = cms.EDFilter("SignatureFilter",
+    src = cms.InputTag("genParticles"),
+    IDs = cms.vint32( 23 ),
+    status = cms.vint32(3),
+    daughters = cms.VPSet(
+        cms.PSet(
+            IDs = cms.vint32( 15 ),
+            status = cms.vint32(3)
+        ),
+        cms.PSet(
+            IDs = cms.vint32( -15 ),
+            status = cms.vint32(3)
+        ),
+    )
+)
