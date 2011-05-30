da4e1ba74ae54079ecd65916300b46f75ff03bee
edelhoff <>
2011-05-30 16:13:59 +0000

    added mc PU shape

--- DiLeptonHistograms/python/vertexWeights_cfi.py
+++ DiLeptonHistograms/python/vertexWeights_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 vertexWeights = cms.PSet(
-    mcFile = cms.string('${CMSSW_BASE}/src/SuSyAachen/DiLeptonHistograms/data/PU_dist.root'),
+    mcFile = cms.string('${CMSSW_BASE}/src/SuSyAachen/DiLeptonHistograms/data/mc_PU_dist.root'),
     mcName = cms.string('pileup'),
     dataFile = cms.string('${CMSSW_BASE}/src/SuSyAachen/DiLeptonHistograms/data/PU_dist.root'),
     dataName = cms.string('pileup'),
