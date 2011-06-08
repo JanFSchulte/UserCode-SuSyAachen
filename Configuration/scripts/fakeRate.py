4029ff201b55a9acefb9359dd44b613d0a85c22b
edelhoff <>
2011-06-08 19:05:00 +0000

    stuff

--- Configuration/scripts/fakeRate.py
+++ Configuration/scripts/fakeRate.py
@@ -2,27 +2,26 @@
 '''
 Created on 22.12.2010
 
-@author: daniel sprenger
+@author: daniel sprenger, matthias edelhoff
 '''
 
 from optparse import OptionParser
-from ConfigParser import SafeConfigParser
+from helpers import BetterConfigParser as SafeConfigParser
 from copy import copy
 
 from messageLogger import messageLogger as log
 
 import ROOT
 
-
-def generateFakeRate(cfgFile):
+def generateFakeRate(cfgFiles):
     from time import asctime
     # ROOT
     ROOT.gROOT.Reset()
     rootContainer = []
-
-    log.logInfo("Loading config file %s" % cfgFile)
+    
+    log.logInfo("Loading config files %s" % cfgFiles)
     parser = SafeConfigParser()
-    parser.read(cfgFile)
+    parser.read(cfgFiles)
     variables = [ i[len("variable:"):] for i in parser.sections() if i.startswith("variable:")]
     log.logDebug("variables: %s" % variables)
 
@@ -71,15 +70,30 @@ def generateFakeRate(cfgFile):
     file.write("""#created at %s
 #created from  '%s'  
 
+if not "cms" in globals(): import FWCore.ParameterSet.Config as cms
+
+
 """%(asctime(), fileName.split()))
     file.close()
     writePSet(outputFileName, parser, tree, bins, "Center")
     writePSet(outputFileName, parser, tree, bins, "Upper")
     writePSet(outputFileName, parser, tree, bins, "Lower")
-    return
+#    writePSet(outputFileName, parser, tree, bins, "UpperSys")
+#    writePSet(outputFileName, parser, tree, bins, "LowerSys")
 
-def writePSet(path, parser, tree, bins, name):
-    from ROOT import TEfficiency
+    return outputFileName
+
+def writePSet(path, parser, trees, bins, name):
+    from ROOT import TFile
+    import os
+
+    cache = {}
+    cacheFilePath = ".fakeRateCache" 
+    if os.path.exists(cacheFilePath):
+        cacheFile = open(cacheFilePath,"r")
+        cache.update( eval(cacheFile.read()) )
+        cacheFile.close()
+    
     file = open(path, 'a')
     file.write("%s =  cms.VPSet(\n"%(parser.get("default","outputPSet", raw=True)%name))
 
@@ -92,16 +106,42 @@ def writePSet(path, parser, tree, bins, name):
         selectionTightBin = appendSelection(bin['selection'], selectionTight)
         log.logDebug("loose selection: %s" % selectionLooseBin)
         log.logDebug("tight selection: %s" % selectionTightBin)
-        loose = tree.GetEntries(selectionLooseBin)
-        tight = tree.GetEntries(selectionTightBin)
-        if tight < 100: log.logWarning("small statistics n= %s for: '%s'"%(tight, bin["selection"]))
-        fakeRate = 1.0 / loose * tight
-        if(name == "Upper"):
-          fakeRate = TEfficiency.ClopperPearson(loose, tight, 0.683, True)
-        if(name == "Lower"):
-          fakeRate = TEfficiency.ClopperPearson(loose, tight, 0.683, False)
+        loose = []
+        tight = []
+        weights = []
+        for tree in trees:
+            inputFileName = tree["path"]
+            if not inputFileName in cache:
+                cache[inputFileName] = {}
+            if not "eventsRunOn" in cache[inputFileName]:
+                rootFile = TFile(inputFileName,"read")
+                cache[inputFileName]["eventsRunOn"] = rootFile.FindObjectAny("analysis paths").GetBinContent(1)
+                rootFile.Close()
+            try:
+                weights.append( float(eval(parser.get("weights", inputFileName, default = "1.0"),{"eventsRunOn":cache[inputFileName]["eventsRunOn"]}) ))
+            except:
+                print log.logError("could not convert weight '%s' for '%s'"%(parser.get("weights", inputFileName, default = "1.0"),inputFileName ))
+                weights.append(1.0)
+            if not selectionLooseBin in cache[inputFileName]:
+                cache[inputFileName][selectionLooseBin] = tree["tree"].GetEntries(selectionLooseBin)
+            loose.append( int(cache[inputFileName][selectionLooseBin]) )
+            
+            if not selectionTightBin in cache[inputFileName]:
+                cache[inputFileName][selectionTightBin] = tree["tree"].GetEntries(selectionTightBin)
+            tight.append( int(cache[inputFileName][selectionTightBin]))
+            
+        if sum(tight) < 100: log.logWarning("small statistics n= %s for: '%s'"%(tight, selectionTightBin))
+        
+        (fakeRate, up, low) = getFakeRateWithErrors(tight, loose, weights)
+        log.logDebug("%s: %s/%s = %f +%f-%f (weights:%s)" % (bin['selection'], tight, loose, fakeRate,up,low,weights))
+        if "Upper" in name:
+            fakeRate = up
+            
+        if "Lower" in name:
+            fakeRate=low
+            #
           
-        log.logDebug("%s: %d/%d = %f" % (bin['selection'], tight, loose, fakeRate))
+
 
         # write pset for bin
         file.write("        cms.PSet(\n")
@@ -113,7 +153,41 @@ def writePSet(path, parser, tree, bins, name):
         file.write("        ),\n")
     file.write(")\n")
     file.close()
+    
+    #write cache
+    cacheFile = open(cacheFilePath,"w")
+    cacheFile.write(str(cache)+"\n")
+    cacheFile.close()
 
+def getFakeRateWithErrors(tight, loose, weights):
+    from ROOT import TEfficiency, Double
+    from array import array
+    from math import sqrt
+    assert len(tight) == len(loose) and len(tight) == len(weights), "tight, loose and weights must have same length mave: (%s, %s, %s)"%(len(tight), len(loose), len(weights))
+#    print "****"
+#    #fakeRate = 1.0 / sum([w*n for w,n in zip(loose, weights)]) * sum([w*n for w,n in zip(tight, weights)])
+#    
+#    up = Double(-1)
+#    low = Double(-1)
+#    #uniform prior 
+#    fakerate = TEfficiency.Combine(up, low, len(weights), array("i",tight), array("i",loose),1.0,1.0,0.683, array("d",weights),"mode")
+#    print "%s\t+ %s\t- %s"%(fakerate, float(low), float(up))
+#    
+#    wLow = [1./TEfficiency.ClopperPearson(int(loose[i]), int(tight[i]), 0.683, False)**2 for i in range(len(tight))]
+#    wUp = [1./TEfficiency.ClopperPearson(int(loose[i]), int(tight[i]), 0.683, True)**2 for i in range(len(tight))]
+#    w = [max(low, high) for (low, high) in zip(wLow, wUp)]
+#    fakerate = sum([w[i]*tight[i]*1./loose[i] for i in range(len(tight))])/sum([w[i] for i in range(len(tight))])
+#    low = sqrt(sum(wLow))
+#    up = sqrt(sum(wUp))
+#    print "%s\t+ %s\t- %s"%(fakerate, low, up)
+    #up = TEfficiency.ClopperPearson(int(loose), int(tight), 0.683, True)
+    #low = TEfficiency.ClopperPearson(int(loose), int(tight), 0.683, False)
+    fakerate = sum(tight) * 1./sum(loose)
+    low = TEfficiency.ClopperPearson(int(sum(loose)), int(sum(tight)), 0.683, False)
+    up = TEfficiency.ClopperPearson(int(sum(loose)), int(sum(tight)), 0.683, True)
+    #print "%s\t+ %s\t- %s"%(fakerate, low, up)
+    
+    return (fakerate, up, low)
 
 def appendSelection(selection1, selection2):
     retValue = ""
@@ -126,20 +200,20 @@ def appendSelection(selection1, selection2):
 
     return retValue
 
-
 def getTreeFromFile(fileNames, treePath):
     log.logDebug("Getting tree '%s'\n  from file %s" % (treePath, fileNames.split()))
-
-    tree = ROOT.TChain(treePath)
+    result = []
     for fileName in fileNames.split():
-      tree.Add(fileName)
+		tree = ROOT.TChain(treePath)
+		tree.Add(fileName)
+		if (tree == None):
+			log.logError("Could not get tree '%s'\n  from file %s" % (treePath, fileName))
+			return None
+		else:
+			tree.SetDirectory(0)
 
-    if (tree == None):
-        log.logError("Could not get tree '%s'\n  from file %s" % (treePath, fileNames.split()))
-        return None
-    else:
-        tree.SetDirectory(0)
-        return tree
+		result.append({"tree": tree, "path":fileName})
+    return result
 
 
 # entry point
@@ -148,8 +222,8 @@ if (__name__ == "__main__"):
     parser = OptionParser()
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
                                   help="talk about everything")
-    parser.add_option("-c", "--cfgFile", dest="cfg", action="store", type="string", default="fakeRate.ini",
-                                  help="cfg file to be read")
+    parser.add_option("-c", "--cfgFile", dest="cfg", action="append", default=[],
+                                  help="cfg file(s) to be read")
 
     (opts, args) = parser.parse_args()
     if (opts.verbose):
--- Configuration/scripts/helpers.py
+++ Configuration/scripts/helpers.py
@@ -10,6 +10,7 @@ class BetterConfigParser(ConfigParser.ConfigParser):
 
     def __init__(self, defaults=None):#in python 2.6, dict_type=dict):
         self.__runtimeRepMap = {}
+        self._activeSection = None
         ConfigParser.ConfigParser.__init__(self, defaults)#, dict_type)            
 
     def get(self, section, option, raw = True, default = None, sectionwideReplace = True):
@@ -17,20 +18,25 @@ class BetterConfigParser(ConfigParser.ConfigParser):
         matchedName = None
         for name, item in self.items(section, raw=raw):
             if re.match(name+"$", option) != None:
-              assert matchedName == None, "option '%s' matches '%s' and '%s'"%(option, matchedName, name)
-              matchedName = name
-              result = item
+                assert matchedName == None, "option '%s' matches '%s' and '%s'"%(option, matchedName, name)
+                matchedName = name
+                result = item
         if matchedName == None:
             if not default is None:
                 result = default
             if self.has_option(section, "default"):
                 result = self.get(section, "default", raw=raw)
-#        result = ConfigParser.ConfigParser.get(self, section, matchedName, raw)
-        if sectionwideReplace:
+        if sectionwideReplace and "splitlines" in dir(result):    
             result = "\n".join([self.__replaceSectionwideTemplates(l) for l in result.splitlines()])
-        #not sure if I have created a chicken egg problem here
+            result = result.replace("\<", "<")
+        #not sure if I have created achicken egg problem here
         for entry in self.__runtimeRepMap:
-            result = result.replace(".oO[%s]Oo." % entry, self.__runtimeRepMap[entry])
+            if "replace" in dir(result):
+                result = result.replace(".oO[%s]Oo." % entry, self.__runtimeRepMap[entry])
+            if "__setitem__" in dir(result):
+                for i in range(len(result)):
+                    if "replace" in dir(result[i]):
+                        result[i] = result[i].replace(".oO[%s]Oo." % entry, self.__runtimeRepMap[entry])
         return result
     
 # need to look this syntax up once I am back online...
@@ -56,49 +62,213 @@ class BetterConfigParser(ConfigParser.ConfigParser):
     def __replaceSectionwideTemplates(self, data):
         '''replace <section|option> with get(section,option) recursivly'''
         result = data
-        findExpression = re.compile("((.*)\<([^\>]*)\|([^\>]*)\>(.*))*")
+        findExpression = re.compile("((.*)\<(?<!\\\<)([^\>]*)\|([^\>]*)\>(.*))*")
         groups = findExpression.search(data).groups()
-        if not self.has_section(groups[2]) and not groups[2] == None:
-            print "WARNING: skipping '%s' in '%s' because section could not be found"%(groups[2],data)
-        if not groups == (None, None, None, None, None) and self.has_section(groups[2]): # expression not matched
+        sectionName = groups[2]
+        if not self._activeSection == None and sectionName == "activeSection":
+            sectionName = self._activeSection
+        if not self.has_section(sectionName) and not sectionName == None:
+            print "WARNING: skipping '%s' in '%s' because section could not be found"%(sectionName, data)
+        if not groups == (None, None, None, None, None) and self.has_section(sectionName): # expression not matched
             result = self.__replaceSectionwideTemplates(groups[1])
             
             matchedName = None
-            for name, item in self.items(groups[2], raw=True):
-                if re.match(name+"$", groups[3]) != None:
-                    if item == "passedString": result +=  self.__replaceSectionwideTemplates(groups[3])
-                    else: result +=  self.__replaceSectionwideTemplates(item)
-                    assert matchedName == None, " '<%s|%s>' did not only match '%s' but also '%s'"%(groups[2],groups[3], name, matchedName)
+            for name, item in self.items(sectionName, raw=True):
+                if not "$" in name: name+="$"
+                if re.match(name, groups[3]) != None:
+                    if item == "passedString": result ="%s%s"%(result,  self.__replaceSectionwideTemplates(groups[3]))
+                    else: result ="%s%s"%(result,  self.__replaceSectionwideTemplates(item))                        
+                    assert matchedName == None, " '<%s|%s>' did not only match '%s' but also '%s'"%(sectionName,groups[3], name, matchedName)
                     matchedName = name
                     
             if matchedName == None:
-                if self.has_option(groups[2],"default"):
-                    if self.get(groups[2], "default") == "passedString":
-                        result +=  self.__replaceSectionwideTemplates( groups[3] )
+                if self.has_option(sectionName,"default"):
+                    if self.get(sectionName, "default") == "passedString":
+                        result ="%s%s"%(result, self.__replaceSectionwideTemplates( groups[3] ))
                     else:
-                        result +=  self.__replaceSectionwideTemplates( self.get(groups[2], "default") )
+                        result ="%s%s"%(result, self.__replaceSectionwideTemplates( self.get(sectionName, "default") ))
                 else:
                     #rethrow exception for the poor
-                    result += self.get(groups[2], groups[3])
-            result += self.__replaceSectionwideTemplates(groups[4])
+                    assert self.has_option(sectionName, groups[3]), "could not find '%s' in section '%s' needed in '%s'"%(groups[3], sectionName, data)
+
+                    result ="%s%s"%(result, self.get(sectionName, groups[3]))
+            result ="%s%s"%(result, self.__replaceSectionwideTemplates(groups[4]))
+        if not findExpression.match(result).groups() ==  (None, None, None, None, None): result = self.__replaceSectionwideTemplates(result)
+        assert not result == None, "something went wrong convertig '%s'"%data
         return result
+    
+    def setActiveSection(self, name=None):
+        from copy import deepcopy
+        before = deepcopy(self._activeSection)
+        self._activeSection = name
+        return before
 
 class Section:
     def __init__(self, config, name, defaults = {}):
         self._config = config
         self._name = name
-        self._defaults = defaults
+        self._defaults = {"defaultSections":[]} 
+        self.setDefaults(defaults)
+        self.evalGlobals = {}
     
     def setDefaults(self, defaults):
-        self._defaults.update(defaults)
+        for name, newValue in defaults.iteritems():
+                #print self._defaults[name], newValue
+            if name in self._defaults:
+                self._defaults[name] = self.__convert(newValue, self._defaults[name])
+            else:
+                self._defaults[name] = newValue
+                
+    def __str__(self):
+        result ="Section: '%s'\n"%self._name 
+        for i in self._config.options(self._name):
+            result += "%s = %s\n"%(i, getattr(self,i))
+        for defaultSection in self.defaultSections:
+            result +="From Default '%s'\n"%defaultSection
+            for i in self._config.options("default:%s"%defaultSection):
+                result += "%s = %s\n"%(i, getattr(self,i))
+        return result
+    
+    def __convert(self, read, default):
+        result = None
+        if type(default) in [float, int]:
+            result = eval(read, self.evalGlobals)
+        elif type(default) in [list, bool]:
+            try:
+                result = eval(read, self.evalGlobals)
+                
+            except:
+                result = read.split()
+                for i in range(len(result)):
+                    if i < len(default):
+                        result[i] =  self.__convert(result[i], default[i])
+                    else:   
+                        if len(default) > 0:
+                            result[i] =  self.__convert(result[i], default[-1])
+        else:
+            result = type(default)(read)
+        return result
     
     def __getattr__(self, name):
         if name in self.__dict__:
             return self.__dict__[ name ]
         else:
             assert self._name in self._config.sections(), "Could not find section '%s' while getting '%s'"%(self._name, name)
-            self._defaults.update( eval(self._config.get(self._name, "defaults", default = "{}") ) )
+            activeBefore = self._config.setActiveSection(self._name)
+            if not name =="defaultSections":
+                for defaultSection in self.defaultSections:
+                    if not self._config.get("default:%s"%defaultSection, name) == None: 
+                        self.setDefaults({name:self._config.get("default:%s"%defaultSection, name)})
+            #self._defaults.update( eval(self._config.get(self._name, "defaults", default = "{}") ) )
             default = None
             if name in self._defaults: default = self._defaults[name]
-            return self._config.get(self._name, name, default = default)
+            result = self._config.get(self._name, name, default = default)
+            
+            if type(result) == type(""):
+                if not name in self._defaults or self._defaults[name] == None:
+                    pass                        
+                else:  
+                    result = self.__convert(result, self._defaults[name])
+            self._config.setActiveSection(activeBefore)
+            return result
+
+def orderByExpressions(input, expressions, sortFkt = sorted):
+    from re import match
+    result = []
+    for orderExpr in expressions:
+        for element in input:
+            expr = orderExpr
+            if not "$" in orderExpr:
+                expr +="$"
+            if not match(expr, element) == None:
+                result.append( element )
+    for element in sortFkt(input):
+        if not element in result:
+            result.append(element)
+    return result
+        
+import unittest
+class Test(unittest.TestCase):
+    def setUp(self):
+        file = open("helpers.unittest.ini","w")
+        file.write("""
+
+[default:testBase]
+a = <other|aNumber>
+b = hallo
+
+[default:testSpecial]
+a = 2 
+c = <other|<activeSection|name>>
+
+[other]
+default = config default
+.*Match = matches
+direct = direct
+aNumber = 1
+.*String = mit sauce  
+
+[emptySection]
+nothing = here
+
+[test]
+defaultSections = testBase
+runtimeReplace = replace is: '.oO[replaceMe]Oo.'
+sectionReplace = direct:'<other|direct>' default:'<other|triggerDefault>' match:'<other|triggerMatch>'
+basicStuff = some things 
+number = 1
+floatNumber = 1.1
+floatList = [1.1, 2.3]
+
+[test2]
+defaultSections = testBase testSpecial
+name = someString
+
+""")
+        file.close()
+        self.cfg = BetterConfigParser()
+        self.cfg.read("helpers.unittest.ini")
+        
+    def tearDown(self):
+        from os import remove
+        remove('helpers.unittest.ini')
+        
+    def testBasic(self):
+        self.assertTrue(self.cfg.get("test","basicStuff")=="some things")
+        self.assertTrue(self.cfg.get("other","doesItMatch", default="not used at all")=="matches")
+        self.assertTrue(self.cfg.get("other","notEvenThere", default="direct default")=="config default")
+        self.assertTrue(self.cfg.get("emptySection","notEvenThere", default="direct default")=="direct default")
+        
+    def testRuntimeReplace(self):
+        self.cfg.addRuntimeReplacement("replaceMe", "replaced.")
+        self.assertTrue(self.cfg.get("test","runtimeReplace")=="replace is: 'replaced.'")
+        
+    def testSectionReplace(self):
+        self.assertTrue(self.cfg.get("test","sectionReplace")=="direct:'direct' default:'config default' match:'matches'")
+    
+    def testSection(self):
+        defaults = {
+                    "number":0,
+                    "anotherNumber":42,
+                    "floatNumber":0.,
+                    "floatList":[0.,]
+                    }
+        sec = Section(self.cfg, "test", defaults)
+        self.assertTrue(sec.number == 1 and type(sec.number) == int)
+        self.assertTrue(sec.anotherNumber == 42 and type(sec.anotherNumber) == int)
+        self.assertTrue(sec.floatNumber == 1.1 and type(sec.floatNumber) == float)
+        self.assertTrue(sec.floatList == [1.1,2.3])
+        self.assertTrue(sec.a == "1")
+        self.assertTrue(sec.b == "hallo")
+        
+        sec = Section(self.cfg, "test2", defaults)
+        self.assertTrue(sec.number == 0 and type(sec.number) == int)
+        self.assertTrue(sec.anotherNumber == 42 and type(sec.anotherNumber) == int)
+        self.assertTrue(sec.floatNumber == 0. and type(sec.floatNumber) == float)
+        self.assertTrue(sec.floatList == [0.])
+        self.assertTrue(sec.a == "2")
+        self.assertTrue(sec.b == "hallo")
+        self.assertTrue(sec.c == "mit sauce")
+    
+    
     
