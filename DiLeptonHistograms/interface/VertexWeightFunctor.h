d1c8649df3e75f07b62e45f0077f9b5c62268547
nmohr <>
2011-06-08 14:17:41 +0000

    Add switch to vertex reweighting

--- DiLeptonHistograms/interface/VertexWeightFunctor.h
+++ DiLeptonHistograms/interface/VertexWeightFunctor.h
@@ -23,6 +23,7 @@ public:
             std::string mcName_ = params.getParameter<std::string>("mcName");
             std::string dataFile_ = params.getParameter<std::string>("dataFile");
             std::string dataName_ = params.getParameter<std::string>("dataName");
+            doWeight_ = params.getParameter<bool>("doWeight");
             /*std::cout << mcFile_ << std::endl;
             std::cout << mcName_ << std::endl;
             std::cout << dataFile_ << std::endl;
@@ -31,16 +32,18 @@ public:
   }
   const double operator()(const edm::Event &iEvent){
     //std::cout << LumiWeights_.weight( iEvent ) << std::endl;
-    if (iEvent.isRealData()) return 1.;
+    if (iEvent.isRealData() || !(doWeight_)) return 1.;
     else return LumiWeights_.weight( iEvent );
   }
   const double operator()(const int nVertices){
     //std::cout << LumiWeights_.weight( nVertices ) << std::endl;
-    return LumiWeights_.weight( nVertices );
+    if (!doWeight_) return 1.;
+    else return LumiWeights_.weight( nVertices );
   }
 
 private:
   edm::LumiReWeighting LumiWeights_;
+  bool doWeight_;
 };
 
 #endif /* VERTEXWEIGHTFUNCTOR_H_ */
--- DiLeptonHistograms/python/vertexWeights_cfi.py
+++ DiLeptonHistograms/python/vertexWeights_cfi.py
@@ -5,4 +5,5 @@ vertexWeights = cms.PSet(
     mcName = cms.string('pileup'),
     dataFile = cms.string('${CMSSW_BASE}/src/SuSyAachen/DiLeptonHistograms/data/PU_dist.root'),
     dataName = cms.string('pileup'),
+    doWeight = cms.bool(True),
 )
